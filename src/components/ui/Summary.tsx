f4978123b16976e40d3e3c6b433120227f07b9dd
Adnan Karim <adnan.karim.dev@gmail.com>
2024-11-08 19:16:30 -0700

    changes/dev script add

--- src/components/ui/Summary.tsx
+++ src/components/ui/Summary.tsx
@@ -612,7 +612,7 @@ export default function SummaryTab({
             <Card>
               <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                 <CardTitle className="text-sm font-medium">
-                  {"5 Star Reviews Posted to Google"}
+                  {"5 Star Reviews Routed to Google"}
                 </CardTitle>
                 <FaGoogle className="h-4 w-4 text-muted-foreground" size={16} />
               </CardHeader>
